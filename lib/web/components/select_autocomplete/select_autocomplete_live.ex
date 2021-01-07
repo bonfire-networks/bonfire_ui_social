2b2d97aafed2b1b1ec9e147e8b57318994332d82
bernini <bernini@inventati.org>
2021-01-07 17:32:05 +0100

    ui

--- lib/web/components/select_autocomplete/select_autocomplete_live.ex
+++ lib/web/components/select_autocomplete/select_autocomplete_live.ex
@@ -2,27 +2,50 @@ defmodule Bonfire.UI.Social.SelectAutocompleteLive do
   use Bonfire.Web, :live_component
 
   def mount(socket) do
-    {:ok, assign(socket, query: nil, result: nil, loading: false, matches: [])}
+    {:ok, assign(socket, 
+      search_results: [],
+      search_phrase: ""
+      )}
   end
 
-  def handle_event("suggest", %{"q" => query}, socket) when byte_size(query) <= 100 do
-    {:noreply, assign(socket, matches: suggest(Enum.map(socket.assigns.locations, fn x -> x.name end), query))}
+  # def handle_event("suggest", %{"q" => query}, socket) when byte_size(query) <= 100 do
+  #   {:noreply, assign(socket, matches: suggest(Enum.map(socket.assigns.locations, fn x -> x.name end), query))}
+  # end
+
+
+  # def handle_event("search", %{"q" => query}, socket) when byte_size(query) <= 100 do
+  #   send(self(), {:search, query})
+  #   {:noreply, assign(socket, query: query, result: "Searching...", loading: true, matches: [])}
+  # end
+
+  # def handle_info({:search, query}, socket) do
+  #   {result, _} = System.cmd("dict", ["#{query}"], stderr_to_stdout: true)
+  #   {:noreply, assign(socket, loading: false, result: result, matches: [])}
+  # end
+  def handle_event("search", %{"search_phrase" => search_phrase}, socket) do
+    IO.inspect(socket)
+    assigns = [
+      search_results: search(Enum.map(socket.assigns.locations, fn x -> x.name end), search_phrase),
+      search_phrase: search_phrase
+    ]
+
+    {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("pick", %{"name" => search_phrase}, socket) do
+    assigns = [
+      search_results: [],
+      search_phrase: search_phrase
+    ]
+
+    {:noreply, assign(socket, assigns)}
   end
 
 
-  def handle_event("search", %{"q" => query}, socket) when byte_size(query) <= 100 do
-    send(self(), {:search, query})
-    {:noreply, assign(socket, query: query, result: "Searching...", loading: true, matches: [])}
-  end
 
-  def handle_info({:search, query}, socket) do
-    {result, _} = System.cmd("dict", ["#{query}"], stderr_to_stdout: true)
-    {:noreply, assign(socket, loading: false, result: result, matches: [])}
-  end
+  def search(""), do: []
 
-  def suggest(""), do: []
-
-  def suggest(list, prefix) do
+  def search(list, prefix) do
     Enum.filter(list, &has_prefix?(&1, prefix))
   end
 
