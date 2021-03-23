2c794f69a864a33e536ed46301f457474a46c6ff
Mayel <git@mayel.space>
2021-03-23 17:00:04 +0100

    follows

---
+++ lib/web/components/followed/followed_live.ex
@@ -0,0 +1,9 @@
+defmodule Bonfire.UI.Social.FollowedLive do
+  use Bonfire.Web, :live_component
+
+  def update(assigns, socket) do
+    followers = Bonfire.Social.Follows.list_followers(e(assigns, :user, nil), e(assigns, :current_user, nil)) |> IO.inspect
+
+    {:ok, assign(socket, %{follows: followers})}
+  end
+end
