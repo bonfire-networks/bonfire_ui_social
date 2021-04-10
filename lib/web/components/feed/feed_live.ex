a672e3dc2e559277a02fca9a29cc31b6a7427d11
Mayel <git@mayel.space>
2021-04-10 16:06:46 +0200

    clean

--- lib/web/components/feed/feed_live.ex
+++ lib/web/components/feed/feed_live.ex
@@ -2,4 +2,28 @@ defmodule Bonfire.UI.Social.FeedLive do
   use Bonfire.Web, :live_component
   alias Bonfire.UI.Social.ActivityLive
 
+  def update(%{feed: _} =assigns, socket) do
+
+    {:ok, assign(socket, assigns
+    |> assigns_merge(%{
+      })) }
+  end
+
+
+  def update(assigns, socket) do # default to instance feed if none other was specified
+    feed_id = Bonfire.Social.Feeds.instance_feed_id()
+
+    feed = Bonfire.Social.FeedActivities.feed(feed_id, e(socket.assigns, :current_user, nil))
+
+    title = e(assigns, :feed_title, "Recent activity on this instance")
+
+    {:ok, socket
+    |> assign(
+      feed_title: title,
+      feed_id: feed_id,
+      feed: e(feed, :entries, []),
+      page_info: e(feed, :metadata, [])
+    )}
+  end
+
 end
--- lib/web/components/nav/header/header_live.ex
+++ lib/web/components/nav/header/header_live.ex
@@ -1,5 +1,4 @@
 defmodule Bonfire.UI.Social.HeaderLive do
   use Bonfire.Web, :live_component
   alias Bonfire.UI.Social.ModalLive
-  alias Bonfire.UI.Social.SidebarMobileLive
 end
