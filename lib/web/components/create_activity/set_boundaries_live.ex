df0d31805a0264c85ccbaf1b8df82d1352c83a70
Mayel <git@mayel.space>
2021-04-15 22:18:10 +0200

    circles

--- lib/web/components/create_activity/set_boundaries_live.ex
+++ lib/web/components/create_activity/set_boundaries_live.ex
@@ -1,4 +1,16 @@
 defmodule Bonfire.UI.Social.SetBoundariesLive do
   use Bonfire.Web, :live_component
 
+  def mount(socket) do
+    {:ok, assign(socket,
+      to_circles: [
+        Bonfire.Boundaries.Circles.get_tuple(:guest),
+        Bonfire.Boundaries.Circles.get_tuple(:local),
+        Bonfire.Boundaries.Circles.get_tuple(:admin),
+        Bonfire.Boundaries.Circles.get_tuple(:activity_pub)
+      ]
+    )}
+  end
+
+
 end
