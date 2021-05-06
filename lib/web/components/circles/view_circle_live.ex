87d557be1f0cc45a7f4285206136185c3aa90ce4
Mayel <git@mayel.space>
2021-05-06 18:39:45 +0200

    multi

--- lib/web/components/circles/view_circle_live.ex
+++ lib/web/components/circles/view_circle_live.ex
@@ -7,8 +7,8 @@ defmodule Bonfire.UI.Social.ViewCircleLive do
       with {:ok, circle} <- Bonfire.Me.Users.Circles.get(assigns.id, e(assigns, :current_user, nil)) |> repo().maybe_preload(encircles: [:subject_profile, :subject_character]) do
         IO.inspect(circle)
 
-      followed = Bonfire.Social.Follows.list_followed(e(assigns, :current_user, nil), e(assigns, :current_user, nil)) #|> IO.inspect
-      followers = Bonfire.Social.Follows.list_followers(e(assigns, :current_user, nil), e(assigns, :current_user, nil)) #|> IO.inspect
+      followed = Bonfire.Social.Follows.list_my_followed(e(assigns, :current_user, nil)) #|> IO.inspect
+      followers = Bonfire.Social.Follows.list_my_followers(e(assigns, :current_user, nil)) #|> IO.inspect
 
       {:ok, assign(socket, assigns
       |> assigns_merge(%{
