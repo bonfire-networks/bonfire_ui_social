62423119812915fd57aa68015a06c9050c7e2d5d
Mayel <git@mayel.space>
2021-05-12 13:30:16 +0200

    user selector

--- lib/web/components/multiselect/live_handler.ex
+++ lib/web/components/multiselect/live_handler.ex
@@ -1,8 +1,8 @@
-defmodule Bonfire.UI.Social.MultiselectLive.UserSelectorLive.LiveHandler do
+defmodule Bonfire.UI.Social.MultiselectLive.LiveHandler do
   use Bonfire.Web, :live_handler
 
   def handle_event("select", %{"id" => id, "name"=>name, "field"=>field} = _attrs, socket) when is_binary(id) do
-
+    IO.inspect("selected for #{field} : #{name}")
     # TODO, handle cases when we want to select multiple
     {:noreply,
         socket
--- lib/web/components/multiselect/user_selector/user_selector_live.ex
+++ lib/web/components/multiselect/user_selector/user_selector_live.ex
@@ -2,26 +2,26 @@ defmodule Bonfire.UI.Social.MultiselectLive.UserSelectorLive do
   use Bonfire.Web, :stateless_component
 
   prop form_input_name, :string
+  prop label, :string
   prop pick_event, :string
   prop remove_event, :string
   prop selected_options, :list
 
   def update(assigns, socket) do
-    IO.inspect(assigns)
+    IO.inspect(userSelectorLive: assigns)
 
     current_user = e(assigns, :current_user, nil)
 
     followed = if current_user, do: Bonfire.Social.Follows.list_followed(current_user) #|> IO.inspect
     |> Enum.map(&follow_to_tuple/1), else: []
 
-    IO.inspect(followed)
+    IO.inspect(followed: followed)
 
     preloaded_options = [{e(current_user, :profile, :name, "Me"), e(current_user, :id, "me")}] ++ followed
 
     {:ok, socket
       |> assigns_merge(assigns,
       preloaded_options: preloaded_options,
-      selected_options: e(assigns, :assign_to, [{e(assigns, :agent, :name, e(assigns, :agent, :profile, :name, nil)), e(assigns, :agent, :id, nil)}])
       )}
   end
 
