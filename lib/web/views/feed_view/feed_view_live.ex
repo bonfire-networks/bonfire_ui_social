4dbe42744ed854343f3ae73a1533ea6064d336ee
bernini <bernini@inventati.org>
2021-06-01 13:21:55 +0200

    dark theme and UI customisation

--- lib/web/views/feed_view/feed_view_live.ex
+++ lib/web/views/feed_view/feed_view_live.ex
@@ -1,4 +1,14 @@
 defmodule  Bonfire.UI.Social.FeedViewLive do
-  use Bonfire.Web, :live_component
+  use Bonfire.Web, :stateless_component
+
+  prop page_title, :string, required: true
+  prop page, :string, required: true
+  prop smart_input, :boolean, required: true
+  prop smart_input_placeholder, :string
+  prop smart_input_text, :string
+  prop search_placholder, :string
+  prop feed_title, :string
+  prop user, :map
+  prop following, :map
 
 end
