32471bf330bf18648b6a9e1d791e19693bdb11a9
Mayel <git@mayel.space>
2021-06-19 20:38:05 +0200

    fix comment

--- lib/web/components/thread/comment_live.ex
+++ lib/web/components/thread/comment_live.ex
@@ -1,20 +1,41 @@
-defmodule  Bonfire.UI.Social.CommentLive do
+defmodule Bonfire.UI.Social.CommentLive do
   use Bonfire.Web, :live_component
 
-  def update(assigns, socket) do
 
-    {activity, comment} = Map.pop(assigns.comment, :activity)
+  def update(%{comment: %{__struct__: Bonfire.Data.Social.Activity} = activity} = assigns, socket) do
+
+    # IO.inspect(comment)
+    # {activity, comment} = Map.pop(comment, :activity)
+    # IO.inspect(activity)
     {object, activity} = Map.pop(activity, :object)
 
     {:ok, assign(socket, assigns
-    |> assigns_merge(
+                          |> assigns_merge(prepare(activity, object, activity))
+          )
+    }
+  end
+
+  def update(%{comment: comment} = assigns, socket) do
+
+    # IO.inspect(comment)
+    {activity, comment} = Map.pop(comment, :activity)
+    # IO.inspect(activity)
+    {object, activity} = Map.pop(activity, :object)
+
+    {:ok, assign(socket, assigns
+                          |> assigns_merge(prepare(comment, object, activity))
+          )
+    }
+  end
+
+  def prepare(comment, object, activity) do
+    %{
       activity: activity,
       object: object,
       comment: comment,
       sub_replies_count: e(activity, :replied, :nested_replies_count, 0) + e(activity, :replied, :direct_replies_count, 0),
-      date_ago: date_from_now(assigns.comment),
-      )) }
+      date_ago: date_from_now(comment)
+    }
   end
-
 
 end
