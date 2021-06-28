73581208dbb4496b2f41f6a7101782d5bb7c0a93
Mayel <git@mayel.space>
2021-06-28 22:10:55 +0200

    tombstone for deleted objects

--- lib/web/components/feed_activity/object/unknown/unknown_live.ex
+++ lib/web/components/feed_activity/object/unknown/unknown_live.ex
@@ -2,4 +2,15 @@ defmodule Bonfire.UI.Social.Activity.UnknownLive do
   use Bonfire.Web, :live_component
   import Bonfire.UI.Social.Integration
 
+  def update(%{object: nil} = assigns, socket) do
+    {:ok, socket |>
+      assigns_merge(assigns,
+        object: %{name: l "[Deleted]"}
+      )}
+  end
+
+  def update(assigns, socket) do
+    {:ok, assign(socket, assigns)}
+  end
+
 end
