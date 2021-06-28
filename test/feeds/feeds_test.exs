d2dfbd52b7dbc18a215b30786a2b8ac5e112a62c
bernini <bernini@inventati.org>
2021-06-28 11:10:54 +0200

    ux tests

---
+++ test/feeds/feeds_test.exs
@@ -0,0 +1,86 @@
+defmodule Bonfire.UI.Social.Feeds.Test do
+
+  use Bonfire.Social.ConnCase
+  alias Bonfire.Social.Fake
+  alias Bonfire.Social.Posts
+  alias Bonfire.Social.Follows
+
+
+  describe "Feeds UX" do
+    test "As a user when I navigate on a feed I want to see the first 10 activities" do
+      
+    end
+
+    test "As a user I dont want to see the load more button if there are less than 11 activities" do
+    
+    end
+
+    test "As a user I want to see the load more button if there are more than 11 activities" do
+    
+    end
+
+    test "As a user when I click on load more I want to see next 10 activities below the others" do
+    
+    end
+
+
+    test "As a user when I publish a new post I want to see it appearing at the beginning of the feed without refreshing the page" do
+    
+    end
+
+    test "As a user I want to see the activity boundary" do
+    
+    end
+  
+    test "As a user I want to see the activity total replies" do
+      
+    end
+
+    test "As a user I want to see the activity total boosts" do
+      
+    end
+
+    test "As a user I want to see the activity total likes" do
+      
+    end
+
+    test "As a user I want to see if I already boosted an activity" do
+      
+    end
+
+    test "As a user I want to see if I already liked an activity" do
+      
+    end
+
+    test "As a user I want to see the context a message is replying to" do
+     
+    end
+
+    test "When I click the reply button, I want to navigate to the thread page" do
+    
+    end
+
+    test "When I click the boost button, I want the boosted activity to appear in the timeline without refreshing" do
+    
+    end
+
+    test "When I click the like button, I want to see the liked activity without refreshing" do
+    
+    end
+
+    test "As a user I want to click over the user avatar or name and navigate to their own profile page" do
+    
+    end
+
+    test "As a user I want to click over a user mention within an activity and navigate to their own profile page" do 
+    
+    end
+
+    test "As a user I want to click over a link that is part of an activity body and navigate to that link" do
+    
+    end
+
+  end
+
+
+end
