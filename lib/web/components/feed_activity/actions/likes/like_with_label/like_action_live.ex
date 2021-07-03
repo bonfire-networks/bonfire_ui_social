535c384bee26eb01b14893603a355bed82d2eac7
Mayel <git@mayel.space>
2021-07-03 15:06:10 +0200

    stateful likes

--- lib/web/components/feed_activity/actions/likes/like_with_label/like_action_live.ex
+++ lib/web/components/feed_activity/actions/likes/like_with_label/like_action_live.ex
@@ -1,6 +1,17 @@
 defmodule Bonfire.UI.Social.LikeActionLive do
+<<<<<<< HEAD:lib/web/components/feed_activity/actions/likes/like_with_label/like_action_live.ex
   use Bonfire.Web, :stateless_component
   prop object_id, :string
   prop my_like, :map
   prop like_count, :map
+=======
+  use Bonfire.Web, :stateful_component
+
+  prop object_id, :string
+  prop liked, :list
+  prop my_like, :any
+
+  def preload(list_of_assigns), do: Bonfire.Social.Likes.LiveHandler.preload(list_of_assigns)
+
+>>>>>>> 1221fb1 (stateful likes):lib/web/components/likes/like_with_label/like_action_live.ex
 end
--- lib/web/components/feed_activity/actions/likes/like_without_label/like_action_without_label_live.ex
+++ lib/web/components/feed_activity/actions/likes/like_without_label/like_action_without_label_live.ex
@@ -1,5 +1,10 @@
 defmodule Bonfire.UI.Social.LikeActionWithoutLabelLive do
-  use Bonfire.Web, :stateless_component
+  use Bonfire.Web, :stateful_component
 
   prop object_id, :string
+  prop liked, :list
+  prop my_like, :any
+
+  def preload(list_of_assigns), do: Bonfire.Social.Likes.LiveHandler.preload(list_of_assigns)
+
 end
