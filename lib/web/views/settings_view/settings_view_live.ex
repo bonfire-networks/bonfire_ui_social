9b75644db92abd83a62a170850643db5684adce1
Mayel <git@mayel.space>
2021-07-10 19:35:21 +0200

    upload

--- lib/web/views/settings_view/settings_view_live.ex
+++ lib/web/views/settings_view/settings_view_live.ex
@@ -1,6 +1,7 @@
 
 defmodule  Bonfire.UI.Social.SettingsViewLive do
   use Bonfire.Web, :live_component
+
   alias Bonfire.UI.Social.SettingsViewsLive.{
     SettingsNavigationLive,
     EditProfileLive,
@@ -8,15 +9,5 @@ defmodule  Bonfire.UI.Social.SettingsViewLive do
     EditAccountLive,
     AdminLive
   }
-  # def update(assigns, socket) do
-
-
-  #   {:ok, assign(socket, assigns
-  #   |> assigns_merge(%{
-  #       page: "Discussion",
-  #       date_ago: date_from_now(assigns.object),
-  #     })) }
-  # end
-
 
 end
