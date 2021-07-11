8b67728875562ef90c209cab06ddf53d2ceaf4b9
bernini <bernini@inventati.org>
2021-07-11 12:22:10 +0200

    tests

---
+++ test/browse/browse_test.exs
@@ -0,0 +1,22 @@
+defmodule Bonfire.UI.Social.Browse.BrowseTest do
+
+  use Bonfire.UI.Social.ConnCase
+  # alias Bonfire.Social.Fake
+  alias Bonfire.Social.{Boosts, Likes, Follows, Posts}
+
+
+  describe "Browse UX:" do
+    
+    test "As a user when I click on Dashboard link, I want to see as default only the activities that belong to my feed" do
+    end
+
+    test "As a user I want to navigate from my feed tab to the instance tab and see the first 10 instance activities" do
+    end
+
+    test "As a user I want to navigate from my feed tab to the fediverse tab and see the first 10 fediverse activities" do
+    end
+
+  end
+
+
+end
--- test/feeds/feed_activity_test.exs
+++ test/feeds/feed_activity_test.exs
@@ -5,12 +5,7 @@ defmodule Bonfire.UI.Social.Feeds.FeedActivityTest do
   alias Bonfire.Social.{Boosts, Likes, Follows, Posts}
 
 
-  describe "Feed UX:" do
-
-    @tag :todo
-    test "As a user I want to see the activity boundary" do
-
-    end
+  describe "Feed: Activity actions" do
 
     test "As a user I want to see the activity total replies" do
       # Create alice user
@@ -89,14 +84,194 @@ defmodule Bonfire.UI.Social.Feeds.FeedActivityTest do
 
     end
 
-    test "As a user I want to see the context a message is replying to" do
+    test "As a user, when I click on reply I am redirected to the activity discussion page" do
+    end
+
+    test "As a user, when I like an activity the counter should increment" do
+    end
+
+    test "As a user, when I like an activity, the label should change from like to liked" do
+    end
+
+    test "As a user when I unlike an activity, the counter should decrement" do
+    end
+
+    test "As a user, when I unlike an activity, the label should change from liked to like" do
+    end
+
+    test "As a user, when I boost an activity, the counter should increment" do
+    end
+
+    test "As a user, when I boost an activity, the label should change from boost to boosted" do
+    end
+ 
+    test "As a user, when I unboost an activity, the counter should decrement" do
+    end
+
+    test "As a user, when I unboost an activity, the label should change from boosted to boost" do
+    end
+
+
+    # As a Guest
+    ## If the user is not logged, we should show a modal when the user clicks on boosts and likes because they may be logged with another server.
+  end
+
+  describe "Feed: Check if the activity is rendered as expected" do
+
+    test "As a user, when I create a new post, I want to see my avatar image in the activity subject" do
 
     end
+
+    test "As a user, when I create a new post, I want to see my name in the activity subject" do
+
+    end
+
+    test "As a user, when I create a new post, I want to see my username next to my name in the activity subject" do
+
+    end
+
+    test "As a user, when I create a new post, I want to see the content as the activity object" do
+
+    end
+
+    test "As a user, when I create a new post, I want to see the reply, boost and like buttons as the activity actions" do
+
+    end
+
+    test "As a user, when I create a new post, I want to see when the post was created" do
+
+    end
+
+    test "As a user I want to see the activity boundary" do
+
+    end
+
+    # When an activity is a reply to another one, in the feed I want to see both activities: the original activity and the reply with enough information to understand the context 
+    test "As a user, when someone replies to an activity, I want to see the author' name of the reply" do
+    
+    end
+
+    test "As a user, when someone replies to an activity, I want to see the reply message" do
+    
+    end
+
+
+    test "As a user, when someone replies to an activity, I want to see the subject of the original activity" do
+    
+    end
+
+    test "As a user, when someone replies to an activity, I want to see the object of the original activity" do
+    
+    end
+
 
     test "As a user I want to click over a link that is part of an activity body and navigate to that link" do
 
     end
 
+  end
+
+  describe "Feed: Boosted activities" do
+    # Boosted activities
+
+    test "As a user, when I boost a post, I want to see the author of the boost the post" do
+    
+    end
+
+    test "As a user, when I boost a post, I want to see the activity boosted subject" do
+
+    end
+
+    test "As a user, when I boosts a post, I want to see the activity boosted object" do
+    
+    end
+
+    test "As a user, when I boosts a post, I want to see the activity boosted action" do
+    
+    end
+
+    test "As a user, when a user I do not follow boosts a post, I want to see the author of the boost the post" do
+    
+    end
+
+    test "As a user, when a user I do not follow boosts a post, I want to see the activity boosted subject" do
+
+    end
+
+    test "As a user, when a user I do not follow boosts a post, I want to see the activity boosted object" do
+    
+    end
+
+    test "As a user, when a user I do not follow boosts a post, I want to see the activity boosted action" do
+    
+    end
+
+    test "As a user, when a user I follow boosts a post, I want to see the author of the boost the post" do
+    
+    end
+
+    test "As a user, when a user I follow boosts a post, I want to see the activity boosted subject" do
+
+    end
+
+    test "As a user, when a user I follow boosts a post, I want to see the activity boosted object" do
+    
+    end
+
+    test "As a user, when a user I follow boosts a post, I want to see the activity boosted action" do
+    
+    end
+
+  end
+
+  describe "Feed: Liked activities" do
+    test "As a user, when I like a post, I want to navigate to my profile, and see the author of the like under liked tab" do
+    
+    end
+
+    test "As a user, when I like a post, I want to navigate to my profile, and see the activity liked subject under liked tab" do
+
+    end
+
+    test "As a user, when I likes a post, I want to navigate to my profile, and see the activity liked object under liked tab" do
+    
+    end
+
+    test "As a user, when I likes a post, I want to navigate to my profile, and see the activity liked actions under liked tab" do
+    
+    end
+
+    test "As a user, when a user I do not follow likes a post, I want to navigate to the user profile, and see the author of the like under liked tab" do
+    
+    end
+
+    test "As a user, when a user I do not follow likes a post, I want to navigate to the user profile, and see the activity liked subject under liked tab" do
+
+    end
+
+    test "As a user, when a user I do not follow likes a post, I want to navigate to the user profile, and see the activity liked object under liked tab" do
+    
+    end
+
+    test "As a user, when a user I do not follow likes a post, I want to navigate to the user profile, and see the activity liked actions under liked tab" do
+    
+    end
+
+    test "As a user, when a user I follow likes a post, I want to navigate to the user profile, and see the author of the like under liked tab" do
+    
+    end
+
+    test "As a user, when a user I follow likes a post, I want to navigate to the user profile, and see the activity liked subject" do
+
+    end
+
+    test "As a user, when a user I follow likes a post, I want to navigate to the user profile, and see the activity liked object" do
+    
+    end
+
+    test "As a user, when a user I follow likes a post, I want to navigate to the user profile, and see the activity liked actions" do
+    
+    end
   end
 
 
