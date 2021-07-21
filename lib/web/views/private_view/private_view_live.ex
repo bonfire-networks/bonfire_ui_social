2c1270090b1728b763230a8573478580ab4e9bcb
bernini <bernini@inventati.org>
2021-07-21 15:39:24 +0200

    localisation

--- lib/web/views/private_view/private_view_live.ex
+++ lib/web/views/private_view/private_view_live.ex
@@ -1,4 +1,8 @@
 defmodule Bonfire.UI.Social.PrivateViewLive do
-  use Bonfire.Web, :live_component
+  use Bonfire.Web, :stateless_component
 
+  prop page_title, :string, required: true
+  prop page, :string, required: true
+  prop smart_input, :boolean, required: true
+  prop feed, :map
 end
