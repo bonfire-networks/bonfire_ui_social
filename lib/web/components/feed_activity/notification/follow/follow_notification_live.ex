131d967eb8cbe65e173a259e76979c742488ba43
bernini <bernini@inventati.org>
2021-10-20 12:04:46 +0200

    notifications

---
+++ lib/web/components/feed_activity/notification/follow/follow_notification_live.ex
@@ -0,0 +1,4 @@
+defmodule Bonfire.UI.Social.Activity.FollowNotificationLive do
+  use Bonfire.Web, :stateless_component
+
+end
---
+++ lib/web/components/feed_activity/notification/like/like_notification_live.ex
@@ -0,0 +1,4 @@
+defmodule Bonfire.UI.Social.Activity.LikeNotificationLive do
+  use Bonfire.Web, :stateless_component
+
+end
