776a98b1ac7b7dc7d93dfaa1f8f50b4183d71cb0
bernini <bernini@inventati.org>
2021-11-23 09:11:14 +0100

    create activity

--- lib/web/components/create_activity/create_activity_live.ex
+++ lib/web/components/create_activity/create_activity_live.ex
@@ -25,7 +25,7 @@ defmodule Bonfire.UI.Social.CreateActivityLive do
         changeset: Bonfire.Social.Posts.LiveHandler.post_changeset(%{}),
         smart_input_placeholder: e(assigns, :smart_input_placeholder, "Write something..."),
         smart_input_text: e(assigns, :smart_input_text, ""),
-        textarea_class: "block w-full h-full h-40 text-sm transition-all duration-300 ease-in-out rounded-none rounded-t resize-none textarea textarea-bordered",
+        textarea_class: "block w-full h-full h-40 text-sm transition-all duration-300 ease-in-out rounded-none rounded-t resize-none textarea",
         with_editor: e(assigns, :showing_within, nil) != :thread && module_enabled?(Bonfire.Editor.Ck.Bubble)
     ))
     |> assign_global(
