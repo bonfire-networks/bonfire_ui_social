ab5b06f4fe6ac19f0eb2f213e75ca8efaea136fe
Mayel de Borniol <git@mayel.space>
2021-11-26 16:59:41 +1300

    search

--- lib/web/components/search/search_live.ex
+++ lib/web/components/search/search_live.ex
@@ -1,17 +1,5 @@
 defmodule Bonfire.UI.Social.SearchLive do
   use Bonfire.Web, :stateless_component
 
-  def render(assigns) do
-    if module_enabled?(Bonfire.Search.Web.FormLive) do
-      ~F"""
-      <Bonfire.Search.Web.FormLive 
-        search_limit=5
-        show_more_link={true}
-      />
-      """
-    else
-      ~F"""
-      """
-    end
-  end
+  prop search, :string
 end
