6745e68b76539874ae69c993a7dc25b677c20b47
Mayel de Borniol <git@mayel.space>
2021-12-11 16:58:53 +1300

    reflow fix

--- lib/web/components/feed_activity/subject/provider_receiver_live.ex
+++ lib/web/components/feed_activity/subject/provider_receiver_live.ex
@@ -8,10 +8,14 @@ defmodule Bonfire.UI.Social.Activity.ProviderReceiverLive do
   prop permalink, :string
 
   def provider(%{provider: provider}), do: provider
-  def provider(%{object: %{provider: provider}}), do: provider
+  def provider(%{object: object}), do: provider(object)
+  def provider(%{activity: activity}), do: provider(activity)
+  def provider(_), do: nil
 
   def receiver(%{receiver: receiver}), do: receiver
-  def receiver(%{object: %{receiver: receiver}}), do: receiver
+  def receiver(%{object: object}), do: receiver(object)
+  def receiver(%{activity: activity}), do: receiver(activity)
+  def receiver(_), do: nil
 
   def profile(object, field), do: e(object, field, nil) |> profile
   def profile(%{profile: %{name: _} = profile}), do: profile
