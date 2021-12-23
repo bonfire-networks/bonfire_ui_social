ef4daa7823cb7f675fd14d05a30dd4bf99800266
Mayel de Borniol <git@mayel.space>
2021-12-23 13:55:14 +1300

    change password

--- lib/web/views/change_password_view/change_password_view_live.ex
+++ lib/web/views/change_password_view/change_password_view_live.ex
@@ -1,4 +1,7 @@
 defmodule  Bonfire.UI.Social.ChangePasswordViewLive do
   use Bonfire.Web, :stateless_component
 
+  prop form, :any
+  prop error, :any, default: ""
+  prop resetting_password, :boolean, default: false
 end
