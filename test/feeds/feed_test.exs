3291b9f1cc968d7ab8d60e7e30fb9e8b34ffe7f9
bernini <bernini@inventati.org>
2022-01-07 17:23:53 +0100

    tests for dogfooding milestone

--- test/feeds/feed_test.exs
+++ test/feeds/feed_test.exs
@@ -7,10 +7,100 @@ defmodule Bonfire.UI.Social.Feeds.FeedActivityTest do
   alias Bonfire.Social.{Boosts, Likes, Follows, Posts}
   alias Bonfire.Repo
 
-  test "As a user I want to see up  to 10 activities when viewing a feed " do
+  def publish_multiple_times(msg, user, n) when n > 0 do
+    {:ok, post} = Posts.publish(user, msg)
+    IO.inspect(n)
+    publish_multiple_times(msg, user, n-1)
+  end
+  
+  def publish_multiple_times(_msg, _user, 0) do
+    :ok
+  end
+
+  test "As a user I want to see the whole amount of activities if they are less than 10" do
+    # Create alice user
+    account = fake_account!()
+    alice = fake_user!(account)
+    # Create bob user
+    account2 = fake_account!()
+    bob = fake_user!(account2)
+
+    total_posts = 8
+    attrs = %{to_circles: [:guest], post_content: %{summary: "summary", name: "test post name", html_body: "first post"}}
+    publish_multiple_times(attrs, bob, total_posts)
+    feed = Bonfire.Social.FeedActivities.my_feed(bob)
+    assigns = [feed: feed.edges, feed_id: "Bob's feed", page_title: "test", page_info: [], showing_within: nil]
+    assert doc = render_surface(Bonfire.UI.Social.FeedViewLive, assigns)
+    assert doc
+       |> Floki.parse_fragment
+       |> elem(1)
+       |> Floki.find("article")
+       |> length == total_posts
+  end
+
+  test "As a user I want to see up to 10 activities when viewing a feed" do
+    # Create alice user
+    account = fake_account!()
+    alice = fake_user!(account)
+    # Create bob user
+    account2 = fake_account!()
+    bob = fake_user!(account2)
+
+    total_posts = 13
+    attrs = %{to_circles: [:guest], post_content: %{summary: "summary", name: "test post name", html_body: "first post"}}
+    publish_multiple_times(attrs, bob, total_posts)
+    feed = Bonfire.Social.FeedActivities.my_feed(bob)
+    assigns = [feed: feed.edges, feed_id: "Bob's feed", page_title: "test", page_info: [], showing_within: nil]
+    assert doc = render_surface(Bonfire.UI.Social.FeedViewLive, assigns)
+    assert doc
+       |> Floki.parse_fragment
+       |> elem(1)
+       |> Floki.find("article")
+       |> length == 10
+  end
+
+  test "As a user I cannot see the load more button if there are less than 10 activities in feed" do
+    # Create alice user
+    account = fake_account!()
+    alice = fake_user!(account)
+    # Create bob user
+    account2 = fake_account!()
+    bob = fake_user!(account2)
+
+    total_posts = 4
+    attrs = %{to_circles: [:guest], post_content: %{summary: "summary", name: "test post name", html_body: "first post"}}
+    publish_multiple_times(attrs, bob, total_posts)
+    feed = Bonfire.Social.FeedActivities.my_feed(bob)
+    IO.inspect(feed.page_info)
+    assigns = [feed: feed.edges, feed_id: "Bob's feed", page_title: "test", page_info: feed.page_info, showing_within: nil]
+    assert doc = render_surface(Bonfire.UI.Social.FeedViewLive, assigns)
+    assert doc
+      |> Floki.parse_fragment
+      |> elem(1)
+      |> Floki.find("[data-id=load_more]")
+      |> Floki.text() =~ ""
   end
 
   test "As a user I want to click on the load more button to load more activities" do
+    # Create alice user
+    account = fake_account!()
+    alice = fake_user!(account)
+    # Create bob user
+    account2 = fake_account!()
+    bob = fake_user!(account2)
+
+    total_posts = 12
+    attrs = %{to_circles: [:guest], post_content: %{summary: "summary", name: "test post name", html_body: "first post"}}
+    publish_multiple_times(attrs, bob, total_posts)
+    feed = Bonfire.Social.FeedActivities.my_feed(bob)
+    IO.inspect(feed.page_info)
+    assigns = [feed: feed.edges, feed_id: "Bob's feed", page_title: "test", page_info: feed.page_info, showing_within: nil]
+    assert doc = render_surface(Bonfire.UI.Social.FeedViewLive, assigns)
+    assert doc
+      |> Floki.parse_fragment
+      |> elem(1)
+      |> Floki.find("[data-id=load_more]")
+      |> Floki.text() =~ "Load more"
   end
 
   test "As a user when I create a new activity, it appears instantly in the feed" do
