fac108887e8f62e90685df64511d282768d7e6a0
bernini <bernini@inventati.org>
2022-01-11 11:23:40 +0100

    added view thread action

---
+++ lib/web/components/feed_activity/actions/view_thread/view_thread_live.ex
@@ -0,0 +1,8 @@
+defmodule Bonfire.UI.Social.Activity.ViewThreadLive do
+  use Bonfire.Web, :stateless_component
+
+  # def handle_event(action, attrs, socket), do: Bonfire.Common.LiveHandlers.handle_event(action, attrs, socket, __MODULE__)
+  prop object_id, :string
+  prop permalink, :string
+  prop replied, :any
+end
