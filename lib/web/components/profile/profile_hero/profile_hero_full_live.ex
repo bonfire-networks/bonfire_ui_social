631de237ee82e288b8a5f250d813b863d8f5b734
Mayel de Borniol <git@mayel.space>
2022-01-17 22:11:10 +1300

    profile tweaks + pw form

--- lib/web/components/profile/profile_hero/profile_hero_full_live.ex
+++ lib/web/components/profile/profile_hero/profile_hero_full_live.ex
@@ -2,4 +2,9 @@ defmodule Bonfire.UI.Social.ProfileHeroFullLive do
   use Bonfire.Web, :stateless_component
 
   prop user, :map
+
+  def display_url("https://"<>url), do: url
+  def display_url("http://"<>url), do: url
+  def display_url(url), do: url
+
 end
