ac2bbcbbd17aca70c5a97dd7a2180e657454c1e4
bernini <bernini@inventati.org>
2022-02-03 22:00:43 +0100

    wip https://github.com/bonfire-networks/bonfire-app/issues/145

---
+++ lib/web/components/nav/user/mobile_user_menu.ex
@@ -0,0 +1,11 @@
+defmodule Bonfire.UI.Social.MobileUserMenuLive do
+  use Bonfire.Web, :stateless_component
+  alias Surface.Components.LivePatch
+  alias Surface.Components.LiveRedirect
+  # prop name, type, options
+  # prop user_image, :string, required: true
+  # prop name, :string, required: true
+  # prop username, :string, required: true
+
+
+end
