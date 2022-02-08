7f1a08296cf03568b556d22adf3f58e3144797df
Mayel de Borniol <git@mayel.space>
2022-02-08 23:27:42 +1300

    hook invite links

--- lib/web/components/settings/admin/invites/admin_invites_live.ex
+++ lib/web/components/settings/admin/invites/admin_invites_live.ex
@@ -1,3 +1,19 @@
 defmodule Bonfire.UI.Social.SettingsViewsLive.AdminInvitesLive do
-  use Bonfire.Web, :stateless_component
+  use Bonfire.Web, :stateful_component
+
+  prop invites, :list, default: []
+
+  def update(assigns, socket) do
+    %{edges: invites, page_info: page_info} = Bonfire.Invite.Links.list_paginated([], socket: socket)
+    {:ok, socket
+    |> assign(
+      invites: invites,
+      page_info: page_info
+    )
+    }
+  end
+
+  def handle_event(action, attrs, socket), do: Bonfire.Common.LiveHandlers.handle_event(action, attrs, socket, __MODULE__)
+  def handle_info(info, socket), do: Bonfire.Common.LiveHandlers.handle_info(info, socket, __MODULE__)
+
 end
