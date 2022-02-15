2e60edad679f20629b210dee6912b40178c736f5
bernini <bernini@inventati.org>
2022-02-15 08:54:07 +0100

    more actions

--- lib/web/components/feed_activity/actions/more_actions_live.ex
+++ lib/web/components/feed_activity/actions/more_actions_live.ex
@@ -1,5 +1,7 @@
 defmodule Bonfire.UI.Social.Activity.MoreActionsLive do
   use Bonfire.Web, :stateless_component
+  import Bonfire.UI.Social.Integration
+
   prop activity, :any
   prop object, :any
   prop flagged, :any
