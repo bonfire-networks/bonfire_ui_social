e03eca1cf1d67c152078397b519a2ae2a88edd43
Mayel de Borniol <git@mayel.space>
2022-02-16 18:53:08 +1300

    reuse btns

--- lib/web/components/feed_activity/actions/flag/flag_action_live.ex
+++ lib/web/components/feed_activity/actions/flag/flag_action_live.ex
@@ -2,7 +2,8 @@ defmodule Bonfire.UI.Social.FlagActionLive do
   use Bonfire.Web, :stateless_component
 
   prop object, :any
-  prop activity, :any
+  prop label, :string
   prop my_flag, :any
+  prop class, :string
 
 end
