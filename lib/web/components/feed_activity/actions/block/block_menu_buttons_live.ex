ea6d1dc84e9ecaabe8b1e9b0bfe6ddd4629d8672
Mayel de Borniol <git@mayel.space>
2022-02-17 21:57:43 +1300

    block btns

---
+++ lib/web/components/feed_activity/actions/block/block_menu_buttons_live.ex
@@ -0,0 +1,8 @@
+defmodule Bonfire.UI.Social.BlockMenuButtonsLive do
+  use Bonfire.Web, :stateless_component
+
+  prop object, :any
+
+  def handle_event(action, attrs, socket), do: Bonfire.Common.LiveHandlers.handle_event(action, attrs, socket, __MODULE__)
+
+end
