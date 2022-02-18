df43ab9744be779107aa7039b42c80df29163a1c
bernini <bernini@inventati.org>
2022-02-18 16:32:11 +0100

    back button optional and css

--- lib/web/components/page_header/page_header_live.ex
+++ lib/web/components/page_header/page_header_live.ex
@@ -2,6 +2,7 @@ defmodule Bonfire.UI.Social.PageHeaderLive do
   use Bonfire.Web, :stateless_component
 
   prop page_title, :string, required: true
+  prop before, :boolean, default: false
   slot default
 
 end
