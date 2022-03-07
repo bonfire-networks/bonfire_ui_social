315fd4bb1b3688c5d3e11d71381bedcc2bd32945
bernini <bernini@inventati.org>
2022-03-07 08:41:17 +0100

    widget message

---
+++ lib/web/components/widget_messages/widget_messages_live.ex
@@ -0,0 +1,25 @@
+defmodule Bonfire.UI.Social.WidgetMessagesLive do
+  use Bonfire.Web, :stateful_component
+
+  def update(assigns, socket) do
+    pagination = %{
+        limit: 3
+      }
+    current_user = current_user(assigns)
+    # feed = Bonfire.Social.FeedActivities.feed(:notifications, Map.put(assigns, :paginate, pagination))
+    feed = if current_user, do: if module_enabled?(Bonfire.Social.Messages), do: Bonfire.Social.Messages.list(current_user, Map.put(%{}, :paginate, pagination)) #|> IO.inspect
+    IO.inspect(feed, label: "FEEEED")
+    {:ok, socket
+      |> assign(
+        feed: e(feed, :edges, []),
+        feed_id: :widget_notifications,
+        page_info: e(feed, :page_info, []),
+        current_user: current_user,
+        showing_within: :widget,
+        feed_update_mode: "prepend",
+        hide_load_more: true
+      )
+    }
+  end
+
+end
