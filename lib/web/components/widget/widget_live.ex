0345f33997dd26e46f952c8d559c83f425369f6c
ivanminutillo <ivanminutillo@outlook.it>
2022-03-11 16:19:01 +0100

    responsive https://github.com/bonfire-networks/bonfire-app/issues/145

--- lib/web/components/widget/widget_live.ex
+++ lib/web/components/widget/widget_live.ex
@@ -2,6 +2,11 @@ defmodule Bonfire.UI.Social.WidgetLive do
   use Bonfire.Web, :stateless_component
   
   prop widget_title, :string
-  slot default
+  
+  @doc "A call to action, usually redirect to the specific page"
+  slot action
+
+  @doc "The main content of the widget"
+  slot default, required: true
 
 end
