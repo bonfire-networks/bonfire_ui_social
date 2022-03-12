fad06bc2cc4c215e3c3323da6eac59987489ca9b
Mayel de Borniol <git@mayel.space>
2022-03-12 13:31:48 +1300

    Remote follows

---
+++ lib/web/components/profile/profile_remote_follow/profile_remote_follow_live.ex
@@ -0,0 +1,15 @@
+defmodule Bonfire.UI.Social.ProfileRemoteFollowLive do
+  use Bonfire.Web, :stateless_component
+
+  prop page_title, :string, required: true
+  prop page, :string, required: true
+  prop selected_tab, :string, default: "timeline"
+  prop smart_input, :boolean, required: true
+  prop has_private_tab, :boolean, required: true
+  prop smart_input_prompt, :string
+  prop smart_input_text, :string
+  prop search_placholder, :string
+  prop feed_title, :string
+  prop user, :map
+
+end
