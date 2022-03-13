0d733d753def38811bfff94850adcc29f5facab5
Mayel de Borniol <git@mayel.space>
2022-03-13 20:01:39 +1300

    LazyImage

--- lib/web/components/feed_activity/subject/avatar_live.ex
+++ lib/web/components/feed_activity/subject/avatar_live.ex
@@ -5,4 +5,15 @@ defmodule Bonfire.UI.Social.Activity.AvatarLive do
   prop profile, :any
   prop viewing_main_object, :boolean
   prop comment, :boolean
+
+  def classes(%{viewing_main_object: true}) do
+    "w-12 h-12"
+  end
+  def classes(%{comment: true}) do
+    "w-6 h-6"
+  end
+  def classes(_) do
+    "w-10 h-10"
+  end
+
 end
