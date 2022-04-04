796e6c6d720705fdf314ead8f32182a37f2ee418
Mayel de Borniol <git@mayel.space>
2022-04-04 12:44:03 +1200

    clean

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -1,14 +1,4 @@
 defmodule Bonfire.UI.Social.Test.FakeHelpers do
 
-  alias Bonfire.Data.Identity.Account
-  alias Bonfire.Me.Fake
-  alias Bonfire.UI.Social.Identity.{Accounts, Users}
-  import ExUnit.Assertions
-
-  import Bonfire.UI.Social.Integration
-
-  require Bonfire.Common.Extend
-  Bonfire.Common.Extend.import_if_enabled(Bonfire.Me.Fake)
-
 
 end
