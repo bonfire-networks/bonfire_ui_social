92937f84cd54366782dd34f72e6ac6514cac0516
Mayel de Borniol <git@mayel.space>
2022-04-17 16:56:57 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/238

--- lib/web/components/nav/header/change_theme_live.ex
+++ lib/web/components/nav/header/change_theme_live.ex
@@ -1,4 +1,5 @@
 defmodule Bonfire.UI.Social.ChangeThemeLive do
   use Bonfire.Web, :stateless_component
 
+  prop selected, :string
 end
---
+++ lib/web/components/settings/settings_items/discoverable_live.ex
@@ -0,0 +1,4 @@
+defmodule Bonfire.UI.Social.SettingsViewsLive.DiscoverableLive do
+  use Bonfire.Web, :stateless_component
+  prop label, :string
+end
