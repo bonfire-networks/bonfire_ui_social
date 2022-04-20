87cb2fe31b7d344f83c2546b3b8fcfd116f16c86
ivanminutillo <ivanminutillo@outlook.it>
2022-04-20 16:51:09 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/192\#issuecomment-1066156671

--- lib/web/components/private/private_live.ex
+++ lib/web/components/private/private_live.ex
@@ -1,5 +1,11 @@
 defmodule Bonfire.UI.Social.PrivateLive do
   use Bonfire.Web, :stateless_component
 
-  prop feed, :any
+  prop feed, :list
+  prop users, :list
+  prop tab_id, :string
+  prop to_circles, :list
+  
+  def handle_event(action, attrs, socket), do: Bonfire.Common.LiveHandlers.handle_event(action, attrs, socket, __MODULE__)
+
 end
--- lib/web/views/private_messages/private_view_live.ex
+++ lib/web/views/private_messages/private_view_live.ex
@@ -3,7 +3,12 @@ defmodule Bonfire.UI.Social.PrivateViewLive do
 
   prop page_title, :string, required: true
   prop page, :string, required: true
-  prop smart_input, :boolean, required: true
+  # prop smart_input, :boolean, required: true
   prop feed, :list
-  prop tab_id, :string, default: nil
+  prop tab_id, :string
+  prop users, :list
+  prop thread_id, :any
+  prop smart_input_prompt, :any
+  prop smart_input_text, :any
+  prop to_circles, :list
 end
