50f4ab6995014b21f01e89a95eb80063a8a87fa8
Mayel de Borniol <git@mayel.space>
2022-05-04 11:02:53 +1200

    errors

--- lib/web/error/error_view.ex
+++ lib/web/error/error_view.ex
@@ -2,42 +2,45 @@ defmodule Bonfire.UI.Social.Web.ErrorView do
   use Bonfire.Web, {:view, [namespace: Bonfire.UI.Social]}
 
   @codes %{
+    404=> "Not allowed",
     404=> "Not found",
     500=> "Something went wrong"
   }
 
-  def render("404.html", _assigns) do
-    show_html(404, "<img src='https://data.whicdn.com/images/304187249/original.gif'/>")
+  def render("403.html", assigns) do
+    show_error(403, reason(assigns)<>"<p><img src='https://media.sciencephoto.com/image/c0021814/800wm'/>", true)
   end
-
-  def render("500.html", %{current_account: current_account, reason: reason}) when not is_nil(current_account) do
-    show_error(500, reason, true)
+  def render("404.html", _assigns) do
+    show_html(404, "<img src='https://i.pinimg.com/originals/98/8d/ef/988def4abdcba22f2c9e907d041a56ce.gif'/>")
   end
   def render("500.html", assigns) do
-    show_error(500, Map.get(assigns, :reason, "Internal error"), true)
+    show_error(500, (reason(assigns) || "Please try again or contact the instance admins.")<>"<p><img src='https://media2.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif'/>", true)
   end
 
-  def render("404.activity+json", assigns) do
+  def render("403.activity+json", assigns) do
+    show_error(403, reason(assigns), false)
+  end
+  def render("404.activity+json", _assigns) do
     show_error(404, nil, false)
   end
   def render("500.activity+json", assigns) do
-    show_error(500, Map.get(assigns, :reason, "Internal error"), false)
+    show_error(500, (reason(assigns) || "Please try again or contact the instance admins."), false)
   end
 
+  def render("403.json", assigns) do
+    show_error(403, reason(assigns), false)
+  end
   def render("404.json", assigns) do
     show_error(404, nil, false)
   end
   def render("500.json", assigns) do
-    show_error(500, Map.get(assigns, :reason, "Internal error"), false)
+    show_error(500, (reason(assigns) || "Please try again or contact the instance admins."), false)
   end
 
   defp show_error(error_code, details, as_html?) do
     error(details)
 
-    details = if error_code !=404 and Bonfire.Common.Config.get!(:env) != :prod, do: reason(details),
-    else: "Please try again or contact the instance admins."
-
-    if as_html?, do: show_html(error_code, "#{details} <img src='https://media2.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif'/>"),
+    if as_html?, do: show_html(error_code, details),
     else: Jason.encode!(%{
       "errors"=> [
         %{
@@ -49,6 +52,7 @@ defmodule Bonfire.UI.Social.Web.ErrorView do
     })
   end
 
+  defp reason(%{reason: reason}), do: reason(reason)
   defp reason(%{message: reason}), do: reason
   defp reason(reason) when is_binary(reason), do: reason
   defp reason(reason), do: inspect reason
@@ -57,7 +61,7 @@ defmodule Bonfire.UI.Social.Web.ErrorView do
   # the template name. For example, "404.html" becomes
   # 404.
   def template_not_found(template, assigns) do
-    error(template)
+    warn(template, "No template defined for")
     show_error(Phoenix.Controller.status_message_from_template(template), Map.get(assigns, :reason, "Unknown Error"), true)
   end
 
@@ -102,7 +106,8 @@ defmodule Bonfire.UI.Social.Web.ErrorView do
         <h1 class="text-primary-content">
           #{error}
         </h1>
-          #{details}
+
+        #{details}
       </div>
     </div>
     </div>
