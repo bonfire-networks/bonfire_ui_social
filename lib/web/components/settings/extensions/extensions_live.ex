8e7bd69c1c6cc63920377f2f33635754097bd47a
Mayel de Borniol <git@mayel.space>
2022-05-10 09:53:14 +1200

    extensions

--- lib/web/components/settings/extensions/extensions_live.ex
+++ lib/web/components/settings/extensions/extensions_live.ex
@@ -2,19 +2,15 @@ defmodule Bonfire.UI.Social.SettingsViewsLive.ExtensionsLive do
   use Bonfire.UI.Common.Web, :stateless_component
   import Bonfire.Common.Extensions
 
-  prop extensions, :list, default: []
-  prop schemas, :any
-  prop other_deps, :any
+  prop data, :list, default: []
+  prop feature_extensions, :list, default: []
+  prop ui, :list, default: []
+  prop schemas, :list, default: []
+  prop ecosystem_libs, :list, default: []
+  prop other_deps, :list, default: []
   prop get_link, :any
   prop settings_section_title, :string, default: "Bonfire extensions"
   prop selected_tab, :string
   prop ading_tab, :string
-
-  def update(assigns, socket) do
-
-    {:ok, assign(socket,
-      assigns_merge(assigns, data())
-    )}
-  end
 
 end
