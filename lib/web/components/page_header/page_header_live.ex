ecb5d07e65ef8ee5a75e5fa81f1fceed8019d9bb
Mayel de Borniol <git@mayel.space>
2022-05-10 10:38:43 +1200

    PageHeaderLive

--- lib/web/components/page_header/page_header_live.ex
+++
@@ -1,8 +0,0 @@
-defmodule Bonfire.UI.Social.PageHeaderLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop page_title, :string, required: true
-  prop before, :boolean, default: false, required: false
-  slot default
-
-end
