8f1269793d86a9ff57de4e70e273dc1fd5dd87cf
Mayel de Borniol <git@mayel.space>
2022-05-11 11:02:26 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/295

--- lib/web/components/settings/admin/instance_info_live.ex
+++ lib/web/components/settings/admin/instance_info_live.ex
@@ -1,8 +1,7 @@
-defmodule Bonfire.UI.Social.SettingsViewsLive.AdminInstanceLive do
+defmodule Bonfire.UI.Social.SettingsViewsLive.InstanceInfoLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop selected_tab, :string
   prop admin_tab, :string
   prop uploads, :any
-
 end
