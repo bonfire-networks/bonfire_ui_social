e2fec669432c0a5898bec07e39967089184328ba
Mayel de Borniol <git@mayel.space>
2022-05-11 09:43:06 +1200

    invites

--- lib/web/components/paginate/load_more_live.ex
+++
@@ -1,9 +0,0 @@
-defmodule Bonfire.UI.Social.LoadMoreLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop live_handler, :string
-  prop page_info, :map
-  prop target, :any
-  prop context, :any
-
-end
