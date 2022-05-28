9f7540fc0e0a5f67bcf3fab37ef46d234791b11c
Mayel de Borniol <git@mayel.space>
2022-05-28 18:03:34 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/281

--- lib/components/activity/object/note/note_live.ex
+++ lib/components/activity/object/note/note_live.ex
@@ -1,7 +1,7 @@
 defmodule Bonfire.UI.Social.Activity.NoteLive do
   use Bonfire.UI.Common.Web, :stateless_component
   alias Bonfire.Common.Text
-  
+
   prop object, :any
   prop activity, :any
   prop viewing_main_object, :boolean
@@ -13,5 +13,9 @@ defmodule Bonfire.UI.Social.Activity.NoteLive do
   def post_content(object) do
     e(object, :post_content, object)
     # |> debug("activity_note_object")
+  end
+
+  def maybe_truncate(input, assigns, length \\ 100) do
+    if e(assigns, :activity_inception, nil), do: Text.sentence_truncate(input, length, "..."), else: input
   end
 end
