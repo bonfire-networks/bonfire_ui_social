2b29a1676425263f2e18cc205e794b508a9a500a
ivanminutillo <ivanminutillo@outlook.it>
2022-06-05 17:30:44 +0200

    responsive

--- lib/views/feeds/feed_view_live.ex
+++ lib/views/feeds/feed_view_live.ex
@@ -9,6 +9,7 @@ defmodule Bonfire.UI.Social.FeedViewLive do
   prop feed_id, :string, required: true
   prop feed_ids, :any
   prop page_info, :any
+  prop page, :string
   prop showing_within, :any, default: nil
   prop verb_default, :string
 
