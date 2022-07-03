fd795b1fbab928d26287c8412a1717bd7869b40c
Mayel de Borniol <git@mayel.space>
2022-07-04 10:19:33 +1200

    fix

--- lib/components/activity/media/media_live.ex
+++ lib/components/activity/media/media_live.ex
@@ -5,14 +5,25 @@ defmodule Bonfire.UI.Social.Activity.MediaLive do
   prop showing_within, :any
 
   def preview_img(media) do
-    e(media.metadata, "facebook", "og:image", nil) || e(media.metadata, "twitter", "twitter:image", nil) || e(media.metadata, "oembed", "thumbnail_url", nil) || media_img(media)
+    ( e(media.metadata, "facebook", "og:image", nil) || e(media.metadata, "twitter", "twitter:image", nil) || e(media.metadata, "oembed", "thumbnail_url", nil) || media_img(media) )
+    |> as_string()
   end
 
   def media_img(media) do
-    e(media.metadata, "oembed", "url", nil) || media_url(media)
+    ( e(media.metadata, "oembed", "url", nil) || media_url(media) )
+    |> as_string()
   end
 
   def media_label(media) do
-    e(media.metadata, "label", nil) || e(media.metadata, "oembed", "title", nil) || e(media.metadata, "facebook", "og:title", nil) || e(media.metadata, "twitter", "twitter:title", nil) || e(media.metadata, "other", "title", nil)
+    ( e(media.metadata, "label", nil) || e(media.metadata, "oembed", "title", nil) || e(media.metadata, "facebook", "og:title", nil) || e(media.metadata, "twitter", "twitter:title", nil) || e(media.metadata, "other", "title", nil) )
+    |> as_string()
   end
+
+  def as_string(list) when is_list(list) do
+    List.first(list) |> as_string()
+  end
+  def as_string(other) do
+    to_string(other)
+  end
+
 end
