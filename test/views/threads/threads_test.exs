cdc1f1858b6ac9a18c97361a72c4f2523bee0dc0
ivanminutillo <ivanminutillo@outlook.it>
2022-11-25 17:20:25 +0100

    ui

---
+++ test/views/threads/threads_test.exs
@@ -0,0 +1,10 @@
+defmodule Bonfire.Social.Threads.ThreadsTest do
+  use Bonfire.UI.Social.ConnCase, async: true
+  alias Bonfire.Social.Fake
+  alias Bonfire.Social.Boosts
+  alias Bonfire.Social.Likes
+  alias Bonfire.Social.Follows
+  alias Bonfire.Social.Posts
+
+
+end
