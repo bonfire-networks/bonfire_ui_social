4c03c90398845d0c14fd273aff943f4013da31f5
ivanminutillo <ivanminutillo@outlook.it>
2023-01-02 18:09:57 +0100

    ui

--- lib/components/activity/activity_live.ex
+++ lib/components/activity/activity_live.ex
@@ -176,13 +176,14 @@ defmodule Bonfire.UI.Social.ActivityLive do
       tabIndex="0"
       phx-hook={if !@viewing_main_object, do: "PreviewActivity"}
       class={
-        "p-4 activity relative pl-18 group bg-base-100 " <> e(assigns, :class, ""),
+        "p-4 activity relative pl-16 group bg-base-100 flex flex-col " <> e(assigns, :class, ""),
+        "!pl-16": e(assigns, :showing_within, :feed) == :thread and e(assigns, :viewing_main_object, false) == false,
         "!pl-4 cursor-pointer":
           e(assigns, :showing_within, :feed) in [:feed, :notifications, :likes, :search],
         "activity_inception bg-base-content/10 !m-0 opacity-100 before:!left-2 before:top-1 before:bottom-1":
           e(assigns, :activity_inception, nil) != nil and e(assigns, :thread_mode, nil) == :flat,
         # showing a quoted reply_to
-        "main_reply_to !rounded-none !shadow-none mb-2 p-2 py-0 mt-2 relative before:absolute before:content-[''] before:w-1 before:bg-base-content/40 before:left-0 before:top-0 before:bottom-0 opacity-60 !border-none":
+        "main_reply_to !mb-1 items-center !flex-row order-first opacity-70 !p-0 !ml-[52px] !pl-2 before:border-neutral-content/80":
           ulid(@object) != nil and e(@activity, :replied, :reply_to_id, nil) == nil and
             ulid(@activity) == nil and @showing_within != :widget and
             @showing_within != :search,
