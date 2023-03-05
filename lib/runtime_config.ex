42523ec2a2c525c105a316983f6741f3129f2f06
Mayel de Borniol <git@mayel.space>
2023-03-06 10:09:34 +1300

    Move UI config to runtime

---
+++ lib/runtime_config.ex
@@ -0,0 +1,48 @@
+defmodule Bonfire.UI.Social.RuntimeConfig do
+  use Bonfire.Common.Localise
+
+  @behaviour Bonfire.Common.ConfigModule
+  def config_module, do: true
+
+  @doc """
+  NOTE: you can override this default config in your app's `runtime.exs`, by placing similarly-named config keys below the `Bonfire.Common.Config.LoadExtensionsConfig.load_configs()` line
+  """
+  def config do
+    import Config
+
+    config :bonfire_ui_social,
+      disabled: false
+
+    config :bonfire, :ui,
+      profile: [
+        # TODO: make dynamic based on active extensions
+        sections: [
+          timeline: Bonfire.UI.Social.ProfileTimelineLive,
+          # private: Bonfire.UI.Social.MessageThreadsLive,
+          posts: Bonfire.UI.Social.ProfilePostsLive,
+          boosts: Bonfire.UI.Social.ProfileBoostsLive,
+          followers: Bonfire.UI.Social.ProfileFollowsLive,
+          followed: Bonfire.UI.Social.ProfileFollowsLive,
+          requested: Bonfire.UI.Social.ProfileFollowsLive,
+          requests: Bonfire.UI.Social.ProfileFollowsLive
+        ],
+        navigation: [
+          timeline: "Timeline",
+          posts: "Posts",
+          boosts: "Boosts"
+          # private: "private",
+        ],
+        network: [
+          followers: "Followers",
+          followed: "Followed"
+        ],
+        my_network: [
+          followers: "Followers",
+          # requests: "Follower requests",
+          followed: "Followed"
+          # requested: "Pending"
+        ],
+        widgets: []
+      ]
+  end
+end
