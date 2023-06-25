5a72ec566ac30fa2dbf056e15e86416d56bb2226
Mayel de Borniol <git@mayel.space>
2023-06-25 21:32:48 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/390

--- lib/components/activity/actions/edit/edit_post_live.ex
+++ lib/components/activity/actions/edit/edit_post_live.ex
@@ -7,4 +7,10 @@ defmodule Bonfire.UI.Social.EditPostLive do
   prop object_boundary, :any, default: nil
 
   prop parent_id, :string, default: nil
+
+  def diff(previous_version, current_version) do
+    # KinoDiff.new(previous_version, current_version, layout: :inline)
+    Exdiff.diff(previous_version, current_version, wrapper_tag: "span")
+    |> Map.get(:html)
+  end
 end
