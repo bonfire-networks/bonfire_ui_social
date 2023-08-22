3d6dfee605657061f12e9759b03d25b140f50381
ivanminutillo <ivanminutillo@outlook.it>
2023-08-22 12:14:47 +0200

    https://github.com/bonfire-networks/bonfire-app/issues/532

--- lib/components/activity/activity_live.ex
+++ lib/components/activity/activity_live.ex
@@ -1188,9 +1188,9 @@ defmodule Bonfire.UI.Social.ActivityLive do
            """
            <div role="status" class="space-y-2.5 animate-pulse max-w-[50%] mb-2">
            <div class="flex items-center w-full space-x-2">
-           <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-10"></div>
-           <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-           <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+           <div class="h-2.5 bg-base-content/10 rounded-full w-10"></div>
+           <div class="h-2.5 bg-base-content/20 rounded-full w-24"></div>
+           <div class="h-2.5 bg-base-content/20 rounded-full w-full"></div>
            </div>
            <span class="sr-only">Loading...</span>
            </div>
