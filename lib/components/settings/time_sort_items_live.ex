cf81977c61d76ab24a11bbe61e6b3bf1a737c17c
ivanminutillo <ivanminutillo@outlook.it>
2023-11-27 15:07:51 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/679

---
+++ lib/components/settings/time_sort_items_live.ex
@@ -0,0 +1,9 @@
+defmodule Bonfire.UI.Social.Settings.TimeSortItemsLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop title, :string
+  prop scope, :any, default: nil
+  prop feed_name, :atom, default: nil
+  prop event_name, :any, default: nil
+  prop event_target, :any, default: nil
+end
