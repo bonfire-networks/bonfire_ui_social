72a6cc18164785a175c077ab4e4dbadc509bbbd9
ivan <ivanminutillo@outlook.it>
2023-12-05 09:48:49 +0100

    ui

--- lib/components/feeds/feed_live.ex
+++ lib/components/feeds/feed_live.ex
@@ -43,7 +43,7 @@ defmodule Bonfire.UI.Social.FeedLive do
 
   prop tab_class, :css_class,
     default:
-      "flex flex-1 pt-4 text-base capitalize hover:bg-base-content hover:bg-opacity-10 place-content-center lined_tab"
+      "flex pt-4 text-base capitalize hover:bg-base-content hover:bg-opacity-10 place-content-center lined_tab"
 
   prop item_class, :css_class,
     default: "text-base-content/60 text-sm pb-3 border-b-4 border-transparent font-medium"
@@ -106,7 +106,7 @@ defmodule Bonfire.UI.Social.FeedLive do
       #     local: l("Local"),
       #     fediverse: l("Remote")
       #   ],
-      #   else: 
+      #   else:
       [explore: l("Everything"), local: l("Local"), fediverse: l("Remote")]
 
       # if context[:current_params]["object_type"] in ["discussions", "posts"],
