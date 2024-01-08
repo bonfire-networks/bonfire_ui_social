d374d298311a7ab153315cca1da6773b4a9e1e4b
ivan <ivanminutillo@outlook.it>
2024-01-08 16:06:08 +0100

    academic paper preview

--- lib/components/activity/media/academic_paper_live.ex
+++ lib/components/activity/media/academic_paper_live.ex
@@ -3,4 +3,9 @@ defmodule Bonfire.UI.Social.Activity.AcademicPaperLive do
   alias Bonfire.UI.Social.Activity.MediaLive
 
   prop media, :map, default: nil
+
+  def update(assign, socket) do
+    debug(socket.assigns.media, "CACCA")
+    {:noreply, socket}
+  end
 end
