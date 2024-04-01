e3227464b53d0a484ab8213dba56618e4c3b4d68
Mayel de Borniol <git@mayel.space>
2024-04-01 15:39:00 +0100

    Optional deps

--- mix.exs
+++ mix.exs
@@ -26,12 +26,7 @@ defmodule Bonfire.UI.Social.MixProject do
       deps: Mess.deps([
         {:phoenix_live_reload, "~> 1.2", only: :dev},
         {:floki, ">= 0.0.0", only: [:dev, :test]},
-        {:bonfire_editor_ck, git: "https://github.com/bonfire-networks/bonfire_editor_ck", branch: "main", optional: true},
-        {:bonfire_editor_quill, git: "https://github.com/bonfire-networks/bonfire_editor_quill", branch: "main", optional: true},
-        {:bonfire_tag, git: "https://github.com/bonfire-networks/bonfire_tag", branch: "main", optional: true},
-        # {:bonfire_boundaries, git: "https://github.com/bonfire-networks/bonfire_boundaries", branch: "main", optional: true}
-        # {:bonfire_valueflows, "https://github.com/bonfire-networks/bonfire_valueflows", branch: "main", optional: true}
-        # {:bonfire_ui_valueflows, "https://github.com/bonfire-networks/bonfire_ui_valueflows", branch: "main", optional: true}
+        {:bonfire_tag, git: "https://github.com/bonfire-networks/bonfire_tag", branch: "main", optional: true, runtime: false}
       ]),
       package: [
         licenses: ["AGPL v3"]
