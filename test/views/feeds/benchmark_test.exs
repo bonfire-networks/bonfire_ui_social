94e29e80ff2915a1f5b5a7a09c7ef4bc276907d5
Mayel de Borniol <git@mayel.space>
2024-05-24 15:26:36 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/924

--- test/views/feeds/benchmark_test.exs
+++ test/views/feeds/benchmark_test.exs
@@ -1,13 +1,13 @@
 defmodule Bonfire.Social.Feeds.Benchmark.Test do
   use Bonfire.UI.Social.ConnCase, async: false
 
-  @tag :benchmark
-  test "feed_queries" do
-    Bonfire.UI.Social.Benchmark.feed_queries()
-  end
+  # @tag :benchmark
+  # test "feed_queries" do
+  #   Bonfire.UI.Social.Benchmark.feed_queries()
+  # end
 
-  @tag :benchmark
-  test "feed_page" do
-    Bonfire.UI.Social.Benchmark.feed_page()
-  end
+  # @tag :benchmark
+  # test "feed_page" do
+  #   Bonfire.UI.Social.Benchmark.feed_page()
+  # end
 end
