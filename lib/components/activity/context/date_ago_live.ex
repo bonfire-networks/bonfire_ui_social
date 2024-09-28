184631e450ba7f6fe00619726a1293d292ad1b64
Mayel de Borniol <git@mayel.space>
2024-09-28 09:49:56 +0100

    surface

--- lib/components/activity/context/date_ago_live.ex
+++ lib/components/activity/context/date_ago_live.ex
@@ -1,6 +1,5 @@
 defmodule Bonfire.UI.Social.Activity.DateAgoLive do
   use Bonfire.UI.Common.Web, :stateless_component
-  # alias Bonfire.Social.Feeds.LiveHandler
 
   prop date_ago, :any, default: nil
   prop object_id, :any, default: nil
