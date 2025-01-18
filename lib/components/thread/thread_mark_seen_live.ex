17514aace486c885e385f4f09a37b306d32f24f9
ivan <ivanminutillo@outlook.it>
2025-01-18 08:10:36 +0100

    tw4

--- lib/components/thread/thread_mark_seen_live.ex
+++
@@ -1,7 +0,0 @@
-defmodule Bonfire.UI.Social.ThreadMarkSeenLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop thread_id, :string, required: true
-  # prop tab, :string, default: nil
-  # prop all_seen, :boolean, default: false
-end
