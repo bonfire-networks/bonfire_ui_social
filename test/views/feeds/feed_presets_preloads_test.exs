c11aad965397bdba7dc8872a4619a6f9009dc8f3
Mayel de Borniol <git@mayel.space>
2025-03-04 09:37:26 +0000

    WIP https://github.com/bonfire-networks/bonfire-app/issues/759

--- test/views/feeds/feed_presets_preloads_test.exs
+++ test/views/feeds/feed_presets_preloads_test.exs
@@ -10,6 +10,7 @@ defmodule Bonfire.UI.Social.PreloadPresetTest do
   alias Bonfire.Social.FeedLoader
   alias Bonfire.Posts
   alias Bonfire.Messages
+  alias Bonfire.Social.Activities
   alias Bonfire.Social.Objects
 
   alias Bonfire.Me.Users
@@ -18,15 +19,21 @@ defmodule Bonfire.UI.Social.PreloadPresetTest do
   import Bonfire.Posts.Fake, except: [fake_remote_user!: 0]
   import Tesla.Mock
 
-  # Get feed presets from config
-  @feed_presets Application.compile_env(:bonfire_social, Bonfire.Social.Feeds)[:feed_presets]
+
+def assert_has_or_open_browser(session, selector, opts \\ []) do 
+  assert_has(session, selector, opts)
+rescue
+  e -> 
+    PhoenixTest.open_browser(session)
+    reraise e, __STACKTRACE__
+end
 
   describe "check preloaded data" do
     setup do
       account = fake_account!()
-      user = fake_user!(account)
-      other_user = fake_user!()
-      third_user = fake_user!()
+      user = fake_user!(account, %{name: "main user", username: "main_user"})
+      other_user = fake_user!("other user")
+      third_user = fake_user!("third user")
 
       conn = conn(user: user, account: account)
 
@@ -35,45 +42,43 @@ defmodule Bonfire.UI.Social.PreloadPresetTest do
     end
 
     # Tests for different feed presets
-    for {preset, preset_config} when preset not in [:user_followers, :user_following] <-
-          @feed_presets do
-      test "for feed preset `#{inspect(preset)}`", %{
+    for %{preset: preset, filters: filters} = params when preset not in [:user_followers, :user_following, :user_by_object_type, :hashtag, :mentions, nil] <-
+          feed_preset_test_params() do
+      test "for `/feed/#{preset}`", %{
         conn: conn,
         account: account,
         user: user,
         other_user: other_user,
         third_user: third_user
       } do
-        # setup do
         preset = unquote(Macro.escape(preset))
-        preset_config = unquote(Macro.escape(preset_config))
+        params = unquote(Macro.escape(params))
 
         # Create test content based on the preset
         {object, activity} = create_test_content(preset, user, other_user)
 
         conn = conn(user: user, account: account)
 
-        {:ok, object: object, activity: activity, preset: preset, preset_config: preset_config}
-        #   end
-
         feed_path = "/feed/#{preset}"
-
-        conn =
-          conn
-          |> visit(feed_path)
-
-          # Basic verification that we're on the right page
-          |> assert_path(feed_path)
 
         # Verify the feed contains our test object
         if object do
           conn =
             conn
-            |> assert_has("[data-object_id=#{id(object)}]")
-            |> assert_has("[data-id=activity]")
+            |> visit(feed_path)
+            |> assert_path(feed_path)
+            |> assert_has_or_open_browser("[data-id=activity]")
+            |> assert_has_or_open_browser("[data-object_id=#{id(object)}]")
+
+          preloads = params.preloads ++ params.postloads
+
+          activity = e(activity, :activity, nil)  || e(object, :activity, nil) || activity
+
+          object = Map.put(object, :activity, activity)
+          |> Activities.activity_preloads(preload: preloads)
 
           # Verify preloads based on the feed preset configuration
-          verify_preloads_in_html(conn, preset_config.filters)
+          verify_preloads_in_html(conn, preloads, object.activity.object || object, object.activity)
         end
       end
     end
@@ -92,76 +97,77 @@ defmodule Bonfire.UI.Social.PreloadPresetTest do
 
       # Visit user's profile feed to see the boost
       conn
-      |> visit("/character/#{user.character.username}")
-      |> assert_has("[data-role=boosted_by]")
-      |> assert_has("[data-verb=Boost]")
+      |> visit("/@#{user.character.username}")
+      |> assert_has_or_open_browser("[data-role=boosted_by]")
+      |> assert_has_or_open_browser("[data-verb=Boost]")
     end
   end
 
   # Helpers to verify different preloads in the HTML
-  defp verify_preloads_in_html(conn, filters) do
-    conn = verify_subject(conn, Enum.member?(filters[:include] || [], :with_subject))
-    conn = verify_creator(conn, Enum.member?(filters[:include] || [], :with_creator))
-    conn = verify_object(conn, Enum.member?(filters[:include] || [], :with_object))
-    conn = verify_object_more(conn, Enum.member?(filters[:include] || [], :with_object_more))
-    conn = verify_media(conn, Enum.member?(filters[:include] || [], :with_media))
-    conn = verify_reply_to(conn, Enum.member?(filters[:include] || [], :with_reply_to))
+  defp verify_preloads_in_html(conn, [], object, activity) do
     conn
   end
+  defp verify_preloads_in_html(conn, expected, object, activity) do
+    conn
+    |> verify_subject(Enum.member?(expected, :with_subject), activity)
+    |> verify_creator(Enum.member?(expected, :with_creator), object)
+    |> verify_object(Enum.member?(expected, :with_object), object)
+    |> verify_media(Enum.member?(expected, :with_media), activity)
+    |> verify_reply_to(Enum.member?(expected, :with_reply_to), activity)
+    # TODO: add verify_peered for :with_peered
+    # TODO: add verify_tags for :tags
+    # TODO: add verify_thread_name for :with_thread_name
+    # TODO: add verify_label for :maybe_with_labelled
+    # TODO: add verify_verb for :with_verb
+  end
 
-  defp verify_subject(conn, expected?) do
+  defp verify_subject(conn, expected?, activity) do
     if expected? do
       conn
-      |> assert_has("[data-id=subject]")
-      |> assert_has("[data-id=subject_avatar]")
-      |> assert_has("[data-id=subject_name]")
+      |> assert_has_or_open_browser("[data-id=subject]")
+      |> assert_has_or_open_browser("[data-id=subject_name]", text: activity.subject.profile.name)
+      # TODO: verify the link matches /@username
     else
       conn
     end
   end
 
-  defp verify_creator(conn, expected?) do
+  defp verify_creator(conn, expected?, object) do
     if expected? do
       conn
       # Creator info is in subject component
-      |> assert_has("[data-id=subject]")
+      |> assert_has_or_open_browser("[data-id=subject_avatar]") 
+      # TODO: verify the link around avatar matches /@username
+      |> assert_has_or_open_browser("[data-id=subject]", text: object.created.creator.profile.name)
     else
       conn
     end
   end
 
-  defp verify_object(conn, expected?) do
+  defp verify_object(conn, expected?, object) do
     if expected? do
       conn
-      |> assert_has("[data-id=activity_note]")
+      |> assert_has_or_open_browser("[data-id=activity_note]", text: object.post_content.html_body)
     else
       conn
     end
   end
 
-  defp verify_object_more(conn, expected?) do
+
+  defp verify_media(conn, expected?, _activity) do
     if expected? do
       conn
-      |> assert_has("[data-id=object_body]")
+      |> assert_has_or_open_browser("[data-id=multimedia_list]")
     else
       conn
     end
   end
 
-  defp verify_media(conn, expected?) do
-    if expected? do
-      conn
-      |> assert_has("[data-id=multimedia_list]")
-    else
-      conn
-    end
-  end
-
-  defp verify_reply_to(conn, expected?) do
+  defp verify_reply_to(conn, expected?, activity) do
     if expected? do
       # In the HTML, a reply typically shows a parent activity above the current activity
       conn
-      |> assert_has(".replied")
+      |> assert_has_or_open_browser(".replied", text: e(activity, :replied, :reply_to, :post_content, nil) )
     else
       conn
     end
