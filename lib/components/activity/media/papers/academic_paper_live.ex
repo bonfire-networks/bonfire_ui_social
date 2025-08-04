7189e949c29bd0b27acb76c9ff36e97a9356124a
ivan <ivanminutillo@outlook.it>
2025-08-04 10:43:02 +0200

    orcid

--- lib/components/activity/media/papers/academic_paper_live.ex
+++ lib/components/activity/media/papers/academic_paper_live.ex
@@ -22,4 +22,36 @@ defmodule Bonfire.UI.Social.Activity.AcademicPaperLive do
       other -> other
     end
   end
+
+  def extract_doi_from_orcid(metadata) when is_map(metadata) do
+    metadata
+    |> e("external-ids", "external-id", [])
+    |> List.wrap()
+    |> Enum.find_value(&extract_doi_from_external_id/1)
+  end
+
+  def extract_doi_from_orcid(_), do: nil
+
+  defp extract_doi_from_external_id(%{"external-id-type" => "doi"} = ext_id) do
+    # Prefer the DOI URL if available, otherwise use the DOI value
+    e(ext_id, "external-id-url", "value", nil) || e(ext_id, "external-id-value", nil)
+  end
+
+  defp extract_doi_from_external_id(_), do: nil
+
+  def is_orcid_work_url?(url) when is_binary(url) do
+    String.contains?(url, "orcid.org/") and String.contains?(url, "/work/")
+  end
+
+  def is_orcid_work_url?(_), do: false
+
+  def format_doi_url(doi_value) when is_binary(doi_value) do
+    if String.starts_with?(doi_value, "http") do
+      doi_value
+    else
+      "https://doi.org/#{doi_value}"
+    end
+  end
+
+  def format_doi_url(_), do: nil
 end
