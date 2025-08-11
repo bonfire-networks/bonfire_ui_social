c50ced44a7e133dd9933edcdae17a0cd24db2891
ivan <ivanminutillo@outlook.it>
2025-08-11 16:58:58 +0200

    theme

--- lib/components/activity/activity_live.ex
+++ lib/components/activity/activity_live.ex
@@ -632,7 +632,7 @@ defmodule Bonfire.UI.Social.ActivityLive do
 
     ~F"""
     <article
-      id={@activity_component_id}
+      id={@activity_component_id || "activity-#{@activity_id || Ecto.UUID.generate()}"}
       data-content-open={!@cw}
       data-object_id={@object_id}
       data-href={@permalink}
