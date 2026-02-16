d2081f5b91de7d4f3dad3c95fc930aff9365e1f4
ivan <ivanminutillo@outlook.it>
2026-02-16 15:00:17 +0100

    ui

--- lib/components/thread/thread_stats_live.ex
+++ lib/components/thread/thread_stats_live.ex
@@ -2,8 +2,8 @@ defmodule Bonfire.UI.Social.Activity.ThreadStatsLive do
   use Bonfire.UI.Common.Web, :stateless_component
   # alias Bonfire.Common.Text
 
-  # prop object, :any
-  # prop activity, :any, default: nil
+  prop object, :any, default: nil
+  prop activity, :any, default: nil
   prop event_target, :any, default: nil
   prop is_remote, :boolean, default: false
   prop reply_count, :any, default: nil
