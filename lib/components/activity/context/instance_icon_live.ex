0730d037c92aee576e7d2f92a344f5d71e7ebe74
Mayel de Borniol <git@mayel.space>
2026-07-31 16:30:21 +0100

    transfer profile to another account capabality for admins

--- lib/components/activity/context/instance_icon_live.ex
+++
@@ -1,6 +0,0 @@
-defmodule Bonfire.UI.Social.Activity.InstanceIconLive do
-  use Bonfire.UI.Common.Web, :stateless_component
-
-  prop object, :any, default: nil
-  prop peered, :any, default: nil
-end
